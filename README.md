91aed80d2b7d22e2f7afea5b9b0c5d74f6d77594
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2015-01-15 15:55:28 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+---
+title: Backups
+description: A Dashing widget for overviewing a backup service
+authors: Taylor Cuilty, Michael Woffendin
+tags: widget, backup, dashing, backups
+created:  2015 Jan 15
+modified: 2015 Jan 15
+
+---
+
+Backups
+=========
+
+![alt tag](https://raw.github.com/osu-sig/Backups-Widget/master/screenshot.png)
+
+## What is it?
+
+The Backups widget is good way to combine tabular and graphical data into a single tile. 
+
+## What kind of data does the bar graph need?
+
+The bar graph assumes you are handing it a number of bytes. It will automatically convert it to a human-friendly format and append the most appropriate units. 
+
+## How do I use it?
+
+The required data format can be a bit confusing, so see the demo job for examples. 
+
+## Are there any dependencies?
+
+Just the number_helpers.js that comes with it. 
---
+++ demo_job.rb
@@ -0,0 +1,37 @@
+bytes_in_gibibyte = 1073741824
+capacity_gb = 65265.3
+used_gb = 30022.0
+
+# Convert everything to bytes so our helpers work properly.
+series = [
+  { values: [ { label: "Capacity", value: used_gb * bytes_in_gibibyte } ],
+    max_value: capacity_gb * bytes_in_gibibyte }
+]
+
+items = [
+  { label: "Space Savings", value: "89.2%"},
+  { label: "Job Failures", value: 3 }
+]
+
+send_event('demo_2', series: series.to_json, items: items)
+
+
+
+
+used_capacity_gb = 27
+total_capacity_gb = 57.1
+used_performance_gb = 8
+total_performance_gb = 13.0
+
+series = [
+  { values: [ { label: "Capacity", value: used_capacity_gb * bytes_in_gibibyte } ], 
+    max_value: total_capacity_gb * bytes_in_gibibyte },
+  { values: [ { label: "Performance", value: used_performance_gb * bytes_in_gibibyte } ], 
+    max_value: total_performance_gb * bytes_in_gibibyte } 
+]
+
+items = [
+  { label: "Compression", value: "12%" }
+]
+
+send_event('demo_1', series: series.to_json, items: items)
